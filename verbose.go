1b6205ab6f45c7ff2e59852feea7a74dada4d7fb
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2016-03-22 10:49:22 +0100

    verbose print funds return n and error (equivalent to fit print funcs)

--- verbose.go
+++ verbose.go
@@ -5,26 +5,42 @@ import (
 	"io"
 )
 
+// Verbose to create simple verbose print api
 type Verbose struct {
 	Active bool
 	Writer io.Writer
 }
 
-func New(w io.Writer, a bool) Verbose {
-	v := Verbose{}
+// New returns a Verbose object.
+// the w parameter set the writer the verbose print funcs write to.
+// if the a parameter was set to true, the print funcs write to the writer.
+func New(w io.Writer, a bool) *Verbose {
+	v := &Verbose{}
 	v.Active = a
 	v.Writer = w
 	return v
 }
 
-func (v *Verbose) Println(a ...interface{}) {
+// Print usage is equivalent to https://golang.org/pkg/fmt/#Print
+func (v *Verbose) Print(a ...interface{}) (n int, err error) {
 	if v.Active {
-		fmt.Fprintln(v.Writer, a...)
+		n, err = fmt.Fprint(v.Writer, a...)
 	}
+	return
 }
 
-func (v *Verbose) Printf(format string, a ...interface{}) {
+// Printf usage is equivalent to https://golang.org/pkg/fmt/#Printf
+func (v *Verbose) Printf(format string, a ...interface{}) (n int, err error) {
 	if v.Active {
-		fmt.Fprintf(v.Writer, format, a...)
+		n, err = fmt.Fprintf(v.Writer, format, a...)
 	}
+	return
+}
+
+// Println usage is equivalent to https://golang.org/pkg/fmt/#Println
+func (v *Verbose) Println(a ...interface{}) (n int, err error) {
+	if v.Active {
+		n, err = fmt.Fprintln(v.Writer, a...)
+	}
+	return
 }
--- verbose_test.go
+++ verbose_test.go
@@ -12,8 +12,30 @@ func Test_Verbose(t *testing.T) {
 	if debug.Active != true {
 		t.Error("Verbose New failed")
 	}
-	debug.Println("hello log")
-	debug.Printf("hello integer %v\n", 1337)
+
+	n, err := debug.Print("hello Print\n")
+	if err != nil {
+		t.Error("Verbose.Print failed")
+	}
+	if n != 12 {
+		t.Error("Verbose.Print returned n not equal")
+	}
+
+	n, err = debug.Printf("hello Printf integer %v\n", 1337)
+	if err != nil {
+		t.Error("Verbose.Printf failed")
+	}
+	if n != 26 {
+		t.Error("Verbose.Printf returned n not equal")
+	}
+
+	n, err = debug.Println("hello Println")
+	if err != nil {
+		t.Error("Verbose.Println failed")
+	}
+	if n != 14 {
+		t.Error("Verbose.Println returned n not equal")
+	}
 
 	t.Log(&buf)
 }
